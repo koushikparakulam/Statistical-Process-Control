550b4861a31f73c83a1de7232556e1fafe0f2496
Koushik Parakulam <koupar12345@gmail.com>
2024-07-26 23:21:27 -0400

    Refactored data module for improved loading

---
+++ Data_Module.py
@@ -0,0 +1,8 @@
+import os
+
+'''ALL FILE PATHS'''
+current_path = os.getcwd()
+pickle_path = current_path + '\\Pickled_Variables'
+
+
+all_pickled_var_names = ['all_sm_norm_permutations', 'all_overlap_norm_permutations']
